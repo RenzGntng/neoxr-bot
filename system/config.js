0294a41588fb9d65bec45a09343e09d089cd321f
RenzGntng <116557415+RenzGntng@users.noreply.github.com>
2022-11-04 12:18:15 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '6283144670363'
 // Owner name
 global.owner_name = 'Customer Service'
 // Maximum upload file size limit (Default : 50 MB)
